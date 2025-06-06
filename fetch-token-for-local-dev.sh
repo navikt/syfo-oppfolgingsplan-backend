643c9c1857a598bd585c65ff5aa41b7c45081c9c
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-06-06 12:42:46 +0200

    script for local dev token

---
+++ fetch-token-for-local-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fetch fake idporten token
+TOKEN=$(curl -s https://fakedings.intern.dev.nav.no/fake/idporten)
+
+# Prepare JSON body
+JSON_BODY=$(jq -n \
+  --arg token "$TOKEN" \
+  '{identity_provider: "tokenx", target: "followupplan-backend", user_token: $token}')
+
+# exchange idporten token for tokenx token
+curl -s -X POST http://localhost:3000/api/v1/token/exchange \
+  -H "Content-Type: application/json" \
+  -d "$JSON_BODY" | jq -r '.access_token'
