8642a4c9e1c472523ba49827dd8cd5c1780d2fe7
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-06-20 15:40:42 +0200

    add initial db schema

---
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE oppfolgingsplan_utkast
+(
+    "uuid"               UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "sykemeldt_fnr"      VARCHAR(11)      NOT NULL,
+    "narmeste_leder_id"  VARCHAR(11)      NOT NULL,
+    "narmeste_leder_fnr" VARCHAR(11)      NOT NULL,
+    "orgnummer"          VARCHAR(9)       NOT NULL,
+    "content"            JSONB            NOT NULL,
+    "sluttdato"          DATE,
+    "created_at"         TIMESTAMP        NOT NULL,
+    "updated_at"         TIMESTAMP        NOT NULL
+);
+
+CREATE TABLE oppfolgingsplan
+(
+    "uuid"                        UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "sykemeldt_fnr"               VARCHAR(11)      NOT NULL,
+    "narmeste_leder_id"           VARCHAR(11)      NOT NULL,
+    "narmeste_leder_fnr"          VARCHAR(11)      NOT NULL,
+    "orgnummer"                   VARCHAR(9)       NOT NULL,
+    "content"                     JSONB            NOT NULL,
+    "sluttdato"                   DATE             NOT NULL,
+    "created_at"                  TIMESTAMP        NOT NULL,
+    "skal_deles_med_lege"         BOOLEAN          NOT NULL,
+    "delt_med_lege_tidspunkt"     TIMESTAMP,
+    "skal_deles_med_veileder"     BOOLEAN          NOT NULL,
+    "delt_med_veileder_tidspunkt" TIMESTAMP
+);
+
+create index utkast_nl_idx on oppfolgingsplan_utkast (narmeste_leder_id);
+create index oppfolgingsplan_nl_idx on oppfolgingsplan (narmeste_leder_id);
+create index oppfolgingsplan_created_at_idx on oppfolgingsplan (created_at);
