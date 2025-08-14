9188c804a93277f7c0b97fcde8b6b07f74b9b518
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-08-14 19:13:41 +0200

    Correct the new name to syfo-oppfolgingsplan-backend (#47)
    Co-authored-by: Bjørn Faaberg <bjof@nav.no>

--- src/main/kotlin/no/nav/syfo/application/metric/Metric.kt
+++ src/main/kotlin/no/nav/syfo/application/metric/Metric.kt
@@ -3,6 +3,6 @@ package no.nav.syfo.application.metric
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 
-const val METRICS_NS = "syfo-oppfolginsplan-backend"
+const val METRICS_NS = "syfo-oppfolgingsplan-backend"
 
 val METRICS_REGISTRY = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
