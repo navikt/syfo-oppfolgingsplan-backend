a3e67c81e7ac16978a694a71d53b3db69a747152
Bjørn Faaberg <bjof@nav.no>
2025-08-28 08:49:20 +0200

    Add length validation on Fodselsnummer

--- src/main/kotlin/no/nav/syfo/oppfolgingsplan/domain/Fodselsnummer.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingsplan/domain/Fodselsnummer.kt
@@ -1,3 +1,9 @@
 package no.nav.syfo.oppfolgingsplan.domain
 
-data class Fodselsnummer(val value: String)
+data class Fodselsnummer(val value: String) {
+    private val elevenDigits = Regex("^\\d{11}\$")
+
+    init {
+        require(elevenDigits.matches(value))
+    }
+}
