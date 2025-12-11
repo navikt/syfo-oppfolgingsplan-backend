6bd107165c5d4cdef98becbe862ace2fbf04e680
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-12-11 07:31:27 +0100

    no put or post when not aktivSykmelding

--- src/main/kotlin/no/nav/syfo/oppfolgingsplan/api/v1/arbeidsgiver/OppfolgingsplanUtkastApiV1.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingsplan/api/v1/arbeidsgiver/OppfolgingsplanUtkastApiV1.kt
@@ -11,6 +11,7 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.put
 import io.ktor.server.routing.route
 import no.nav.syfo.application.auth.BrukerPrincipal
+import no.nav.syfo.application.exception.ForbiddenException
 import no.nav.syfo.application.exception.UnauthorizedException
 import no.nav.syfo.dinesykmeldte.DineSykmeldteService
 import no.nav.syfo.oppfolgingsplan.db.domain.toResponse
@@ -40,6 +41,10 @@ fun Route.registerArbeidsgiverOppfolgingsplanUtkastApiV1(
             }
 
             val sykmeldt = call.attributes[CALL_ATTRIBUTE_SYKMELDT]
+
+            if (sykmeldt.aktivSykmelding != true) {
+                throw ForbiddenException("Sykmeldt does not have an active sykmelding")
+            }
 
             val lagreUtkastResponse = oppfolgingsplanService.persistOppfolgingsplanUtkast(
                 innloggetBruker.ident,
