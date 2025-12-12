e47440cc49b3f0527a9345e3d115f45b46f655db
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-12-12 08:03:04 +0100

    Fix spelling

--- src/main/kotlin/no/nav/syfo/oppfolgingsplan/api/v1/OppfolginsplanAPiMetrics.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingsplan/api/v1/OppfolginsplanAPiMetrics.kt
@@ -4,16 +4,16 @@ import io.micrometer.core.instrument.Counter
 import no.nav.syfo.application.metric.METRICS_NS
 import no.nav.syfo.application.metric.METRICS_REGISTRY
 
-const val OPPFOLGINSPLAN_CREATED = "${METRICS_NS}_oppfolginsplan_created"
-const val OPPFOLGINSPLAN_SHARED_WITH_GP = "${METRICS_NS}_oppfolginsplan_shared_with_gp"
-const val OPPFOLGINSPLAN_SHARED_WITH_NAV = "${METRICS_NS}_oppfolginsplan_shared_with_nav"
+const val OPPFOLGINGSPLAN_CREATED = "${METRICS_NS}_oppfolgingsplan_created"
+const val OPPFOLGINGSPLAN_SHARED_WITH_GP = "${METRICS_NS}_oppfolgingsplan_shared_with_gp"
+const val OPPFOLGINGSPLAN_SHARED_WITH_NAV = "${METRICS_NS}_oppfolgingsplan_shared_with_nav"
 
-val COUNT_OPPFOLGINSPLAN_CREATED: Counter = Counter.builder(OPPFOLGINSPLAN_CREATED)
-    .description("Counts the number of created oppfolginsplans")
+val COUNT_OPPFOLGINGSPLAN_CREATED: Counter = Counter.builder(OPPFOLGINGSPLAN_CREATED)
+    .description("Counts the number of created oppfolgingsplans")
     .register(METRICS_REGISTRY)
-val COUNT_OPPFOLGINSPLAN_SHARED_WITH_GP: Counter = Counter.builder(OPPFOLGINSPLAN_SHARED_WITH_GP)
-    .description("Counts the number of oppfolginsplans that are shared with a doctor")
+val COUNT_OPPFOLGINGSPLAN_SHARED_WITH_GP: Counter = Counter.builder(OPPFOLGINGSPLAN_SHARED_WITH_GP)
+    .description("Counts the number of oppfolgingsplans that are shared with a doctor")
     .register(METRICS_REGISTRY)
-val COUNT_OPPFOLGINSPLAN_SHARED_WITH_NAV: Counter = Counter.builder(OPPFOLGINSPLAN_SHARED_WITH_NAV)
-    .description("Counts the number of created oppfolginsplans")
+val COUNT_OPPFOLGINGSPLAN_SHARED_WITH_NAV: Counter = Counter.builder(OPPFOLGINGSPLAN_SHARED_WITH_NAV)
+    .description("Counts the number of created oppfolgingsplans")
     .register(METRICS_REGISTRY)
