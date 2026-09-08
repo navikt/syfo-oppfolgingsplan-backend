3c02db6a793479ef15975fc61a06a409640deac2
Luan Tran <8613937+luantr@users.noreply.github.com>
2026-09-08 07:43:32 +0000

    chore: grand esyfo analyse access to new tables (#467)
    ## Beskrivelse
    
    <!-- Hva gjør denne PR-en og hvorfor? -->
    
    ## Endringer
    
    <!-- - `fil/modul`: Hva som ble endret -->
    
    ## Issue
    
    <!-- Closes #NUMMER / Relates to #NUMMER -->
    
    ## Sjekkliste
    
    - [ ] Koden kompilerer og linter uten feil
    - [ ] Endringene er testet (manuelt eller automatisk)
    - [ ] Ingen sensitiv data eksponert (tokens, credentials, PII)

--- src/main/resources/db/migration/R__grant_analysis_privileges.sql
+++ src/main/resources/db/migration/R__grant_analysis_privileges.sql
@@ -1,2 +1,3 @@
--- Grant SELECT privileges on all tables in the PUBLIC schema to the esyfo-analyse role
 GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "esyfo-analyse";
+ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC
+    GRANT SELECT ON TABLES TO "esyfo-analyse";
